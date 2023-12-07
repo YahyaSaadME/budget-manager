95ecef360e550b3ba586e50047ea181fb7dd7502
Yahya saad <yahyasaad.me@gmail.com>
2023-12-07 14:49:37 +0530

    Local Done

--- index.js
+++ index.js
@@ -40,77 +40,88 @@ const allLogs = async () => {
   }
 };
 allLogs();
-console.log(document.getElementById("type").value);
 const reset = () => {
   localStorage.removeItem('logs')
   localStorage.removeItem('myWallet')
   location.replace('./add.html')
 };
+
+
 const addLogs = async () => {
   const why = document.getElementById("why").value;
   const Amount = document.getElementById("input-amount").value;
   const type = document.getElementById("type").value;
+  const data = JSON.parse(localStorage.getItem('myWallet'))
 
   if(Amount==0 || Amount =="" || why==""){
     document.getElementById("bad-alert").style.display = "flex";
     
   }else{
-    
-    try {
-    document.getElementById("good-alert").style.display = "flex";
-    let prev = JSON.parse(localStorage.getItem("logs"));
-    let prevAmount = JSON.parse(localStorage.getItem("myWallet"));
-    if (!prev) {
-      prev = [];
-    }
-    if (prevAmount == null) {
-      localStorage.setItem(
-        "myWallet",
-        JSON.stringify({ cash: 0, gpay: 0, spent: 0 })
-      );
-      prevAmount = JSON.parse(localStorage.getItem("myWallet"));
-    }
-    const add = localStorage.setItem(
-      "logs",
-      JSON.stringify([
-        {
-          why: why,
-          amount:Amount,
-          type: type == "Cash" ? "Cash" : "Gpay",
-          when: new Date(),
-        },
-        ...prev,
-      ])
-    );
-    if(type == "Cash"){
-      localStorage.setItem(
-        "myWallet",
-        JSON.stringify({
-          cash: prevAmount.cash - Number(Amount),
-          gpay: prevAmount.gpay,
-          spent: Number(prevAmount.spent) + Number(Amount),
-        })
-      );
+    if(type == "Cash" && Number(Amount)>data.cash){
+        document.getElementById("Amount-exceed-alert").style.display = "flex"
+    }else if(type == "Gpay" && Number(Amount)>data.gpay){
+      document.getElementById("Amount-exceed-alert").style.display = "flex"
     }else{
-      localStorage.setItem(
-        "myWallet",
-        JSON.stringify({
-          gpay: prevAmount.gpay - Number(Amount),
-          cash: prevAmount.cash,
-          spent: Number(prevAmount.spent) + Number(Amount),
-        })
-      );
+      try {
+        document.getElementById("good-alert").style.display = "flex";
+        let prev = JSON.parse(localStorage.getItem("logs"));
+        let prevAmount = JSON.parse(localStorage.getItem("myWallet"));
+        if (!prev) {
+          prev = [];
+        }
+        if (prevAmount == null) {
+          localStorage.setItem(
+            "myWallet",
+            JSON.stringify({ cash: 0, gpay: 0, spent: 0 })
+          );
+          prevAmount = JSON.parse(localStorage.getItem("myWallet"));
+        }
+        const add = localStorage.setItem(
+          "logs",
+          JSON.stringify([
+            {
+              why: why,
+              amount:Amount,
+              type: type == "Cash" ? "Cash" : "Gpay",
+              when: new Date(),
+            },
+            ...prev,
+          ])
+        );
+        if(type == "Cash"){
+          localStorage.setItem(
+            "myWallet",
+            JSON.stringify({
+              cash: prevAmount.cash - Number(Amount),
+              gpay: prevAmount.gpay,
+              spent: Number(prevAmount.spent) + Number(Amount),
+            })
+          );
+        }else{
+          localStorage.setItem(
+            "myWallet",
+            JSON.stringify({
+              gpay: prevAmount.gpay - Number(Amount),
+              cash: prevAmount.cash,
+              spent: Number(prevAmount.spent) + Number(Amount),
+            })
+          );
+        }
+        allLogs();
+      } catch (error) {
+        document.getElementById("good-alert").style.display = "none";
+        document.getElementById("bad-alert").style.display = "flex";
+        console.log(error);
+      }
     }
-    allLogs();
-  } catch (error) {
-    document.getElementById("good-alert").style.display = "none";
-    document.getElementById("bad-alert").style.display = "flex";
-    console.log(error);
-  }
+
 }
 };
+
+
 
 const alertClose = () => {
   document.getElementById("good-alert").style.display = "none";
   document.getElementById("bad-alert").style.display = "none";
+  document.getElementById("Amount-exceed-alert").style.display = "none";
 };
