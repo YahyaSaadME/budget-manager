e1e13057a34d0291cb37bd015758e79e89381976
Yahya saad <yahyasaad.me@gmail.com>
2023-12-08 20:55:10 +0530

    Date feature added

--- index.js
+++ index.js
@@ -15,34 +15,49 @@ const allLogs = async () => {
     if (data != null) {
       document.getElementById("no").style.display = "none";
       document.getElementById("logs").innerHTML = "";
-      
+
       function splitArrayByDate(dataArray) {
         return dataArray.reduce((result, currentObj) => {
           console.log(currentObj.when);
-          let date = new Date(currentObj.when)
-          date = new Date(`${date.getMonth()+1} ${date.getDate()} ${date.getFullYear()}`);
+          let date = new Date(currentObj.when);
+          date = new Date(
+            `${date.getMonth() + 1} ${date.getDate()} ${date.getFullYear()}`
+          );
           if (!result[date]) {
             result[date] = [];
           }
-          result[date].push({why:currentObj.why,type:currentObj.type,amount:currentObj.amount,when:currentObj.when});
+          result[date].push({
+            why: currentObj.why,
+            type: currentObj.type,
+            amount: currentObj.amount,
+            when: currentObj.when,
+            received:currentObj.received
+          });
           return result;
         }, {});
       }
 
-      const splitData= Object.entries(splitArrayByDate(data)).map(([date, data]) => ({ date, data }))
+      const splitData = Object.entries(splitArrayByDate(data)).map(
+        ([date, data]) => ({ date, data })
+      );
 
       splitData.map((data, i) => {
-        console.log(data);
         const date = new Date(data.date);
-        const currentDate = new Date()
-        console.log(currentDate.toDateString()==date.toDateString());
-        if(date.toDateString() == currentDate.toDateString()){
-          document.getElementById("logs").innerHTML += `<div class='center-date-div'><h2 class='center-date'>Today</h2></div>`
-        }else{
-          document.getElementById("logs").innerHTML += `<div class='center-date-div'><h2 class='center-date'>${date.getDate()} | ${date.getMonth()+1} | ${date.getFullYear()} </h2></div>`
+        const currentDate = new Date();
+        if (date.toDateString() == currentDate.toDateString()) {
+          document.getElementById(
+            "logs"
+          ).innerHTML += `<div class='center-date-div'><h2 class='center-date'>Today</h2></div>`;
+        } else {
+          document.getElementById(
+            "logs"
+          ).innerHTML += `<div class='center-date-div'><h2 class='center-date'>${date.getDate()} | ${
+            date.getMonth() + 1
+          } | ${date.getFullYear()} </h2></div>`;
         }
-        data.data.map((e,i)=>{
-          const when = new Date(e.when)
+        data.data.map((e, i) => {
+          const when = new Date(e.when);
+          console.log(e);
           document.getElementById("logs").innerHTML += `
             <div class="log">
           <div>
@@ -54,10 +69,10 @@ const allLogs = async () => {
               <h2 class="time">${when.getHours()}:${when.getMinutes()}:${when.getSeconds()}</h2>
             </div>
           </div>
-          <h2 class="amount">₹ ${e.amount}</h2>
+          <h2 class="amount">${e.received?"+ ":" "} ₹${e.amount} /-</h2>
         </div>
             `;
-        })
+        });
       });
     } else {
       document.getElementById("no").style.display = "flex";
@@ -73,7 +88,79 @@ const reset = () => {
   location.replace("./add.html");
 };
 
-const addLogs = async () => {
+const addSpentLogs = async () => {
+  const why = document.getElementById("why").value;
+  const Amount = document.getElementById("input-amount").value;
+  const type = document.getElementById("type").value;
+  const data = JSON.parse(localStorage.getItem("myWallet"));
+
+  if (Amount == 0 || Amount == "" || why == "") {
+    document.getElementById("bad-alert").style.display = "flex";
+  } else {
+    if (type == "Cash" && Number(Amount) > data.cash) {
+      document.getElementById("Amount-exceed-alert").style.display = "flex";
+    } else if (type == "Gpay" && Number(Amount) > data.gpay) {
+      document.getElementById("Amount-exceed-alert").style.display = "flex";
+    } else {
+      try {
+        document.getElementById("good-alert").style.display = "flex";
+        let prev = JSON.parse(localStorage.getItem("logs"));
+        let prevAmount = JSON.parse(localStorage.getItem("myWallet"));
+        if (!prev) {
+          prev = [];
+        }
+        if (prevAmount == null) {
+          localStorage.setItem(
+            "myWallet",
+            JSON.stringify({ cash: 0, gpay: 0, spent: 0 })
+          );
+          prevAmount = JSON.parse(localStorage.getItem("myWallet"));
+        }
+        const add = localStorage.setItem(
+          "logs",
+          JSON.stringify([
+            {
+              why: why,
+              amount: Amount,
+              type: type == "Cash" ? "Cash" : "Gpay",
+              when: new Date(),
+              received:false
+            },
+            ...prev,
+          ])
+        );
+        if (type == "Cash") {
+          localStorage.setItem(
+            "myWallet",
+            JSON.stringify({
+              cash: prevAmount.cash - Number(Amount),
+              gpay: prevAmount.gpay,
+              spent: Number(prevAmount.spent) + Number(Amount),
+              received:false
+            })
+          );
+        } else {
+          localStorage.setItem(
+            "myWallet",
+            JSON.stringify({
+              gpay: prevAmount.gpay - Number(Amount),
+              cash: prevAmount.cash,
+              spent: Number(prevAmount.spent) + Number(Amount),
+              received:false
+            })
+          );
+        }
+        allLogs();
+      } catch (error) {
+        document.getElementById("good-alert").style.display = "none";
+        document.getElementById("bad-alert").style.display = "flex";
+        console.log(error);
+      }
+    }
+  }
+};
+
+const addReceivedLogs = async () => {
   const why = document.getElementById("why").value;
   const Amount = document.getElementById("input-amount").value;
   const type = document.getElementById("type").value;
@@ -110,6 +197,7 @@ const addLogs = async () => {
               amount: Amount,
               type: type == "Cash" ? "Cash" : "Gpay",
               when: new Date(),
+              received:true
             },
             ...prev,
           ])
@@ -118,18 +206,20 @@ const addLogs = async () => {
           localStorage.setItem(
             "myWallet",
             JSON.stringify({
-              cash: prevAmount.cash - Number(Amount),
+              cash: prevAmount.cash + Number(Amount),
               gpay: prevAmount.gpay,
-              spent: Number(prevAmount.spent) + Number(Amount),
+              spent: Number(prevAmount.spent),
+              received:true
             })
           );
         } else {
           localStorage.setItem(
             "myWallet",
             JSON.stringify({
-              gpay: prevAmount.gpay - Number(Amount),
+              gpay: prevAmount.gpay + Number(Amount),
               cash: prevAmount.cash,
-              spent: Number(prevAmount.spent) + Number(Amount),
+              spent: Number(prevAmount.spent),
+              received:true
             })
           );
         }
@@ -148,3 +238,26 @@ const alertClose = () => {
   document.getElementById("bad-alert").style.display = "none";
   document.getElementById("Amount-exceed-alert").style.display = "none";
 };
+
+document.getElementById("why").addEventListener("click", (e) => {
+  document.getElementById("reason-shortcut").style.display = "grid";
+});
+document.getElementById("input-amount").addEventListener("click", (e) => {
+  document.getElementById("amount-shortcut").style.display = "grid";
+});
+window.addEventListener("click", (e) => {
+  if (e.target.id != "why") {
+    document.getElementById("reason-shortcut").style.display = "none";
+  } 
+  if (e.target.id != "input-amount") {
+    document.getElementById("amount-shortcut").style.display = "none";
+  }
+});
+
+const WSC =(e)=>{
+  document.getElementById("why").value += e
+}
+const ASC =(e)=>{
+  let val = document.getElementById("input-amount").value
+  document.getElementById("input-amount").value = Number(val) + e
+}
